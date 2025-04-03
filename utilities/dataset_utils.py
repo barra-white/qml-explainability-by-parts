65aa586352834054415b67d33fe2e90375dbb488
Barra White <barraedwardwhite@gmail.com>
2025-04-03 15:33:09 +0100

    version change+markdown additions

--- utilities/dataset_utils.py
+++ utilities/dataset_utils.py
@@ -53,7 +53,7 @@ class DiabetesData:
         Loads the Pima Indians Diabetes dataset, performs an 70-30 train-test split while maintaining the class distribution,
         and scales the features using the MinMaxScaler (fitting only on the training data to avoid leakage)
 
-        Parameters:
+        Params:
             train_size (float): Proportion of data to use for training (default: 0.7)
             random_state (int): Seed for reproducibility (default: 42)
             shuffle (bool): Whether to shuffle the dataset before splitting (default: True)
@@ -97,60 +97,3 @@ class DiabetesData:
         Returns the feature names in the Pima Indians Diabetes dataset.
         """
         return self.data.columns[:-1]
-    
-    
-
-
-
-
-
-# def preprocess_data(train_size=0.7, random_state=42, shuffle=True):
-#     """
-#     Loads the breast cancer dataset, performs an 70-30 train-test split while maintaining the class distribution,
-#     and scales the features using the MinMaxScaler (fitting only on the training data to avoid leakage).
-
-#     Parameters:
-#         train_size (float): Proportion of data to use for training (default: 0.7).
-#         random_state (int): Seed for reproducibility (default: 42).
-#         shuffle (bool): Whether to shuffle the dataset before splitting (default: True).
-#     """
-    
-#     # Load the Iris dataset.
-#     wine = load_wine()
-#     X = wine.data
-#     y = wine.target
-    
-#     # encode labels
-#     scaler = MinMaxScaler()
-#     X = scaler.fit_transform(X)
-    
-#     # split the data
-#     X_train, X_test, y_train, y_test = train_test_split(
-#         X, y, train_size=train_size, random_state=random_state, shuffle=shuffle, stratify=y
-#     )
-
-#     return X_train, X_test, y_train, y_test
-
-# def get_num_features() -> int:
-#     """
-#     Returns the number of features in the wine dataset.
-#     """
-#     return load_wine().data.shape[1]
-
-# def get_num_classes() -> int:
-#     """
-#     Returns the number of classes in the wine dataset.
-#     """
-#     return len(load_wine().target_names)
-
-# def get_class_names() -> list[str]:
-#     """
-#     Returns the class names in the wine dataset.
-#     """
-#     return load_wine().target_names
-
-# def get_feature_names() -> list[str]:
-#     """
-#     Returns the feature names in the wine dataset.
-#     """
-#     return load_wine().feature_names
